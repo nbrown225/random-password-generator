89e98eb89d5e9fe93e83c3b92961e316bb1db1d2
nbrown225 <chbrown225@yahoo.com>
2022-08-07 17:55:27 -0700

    random numbers letters and characters functions working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,91 @@
+// GET DOM ELEMENTS
 var startBtn = document.querySelector('#generate')
-startFunc = () => {
-    prompt("Please enter some text",
-    "GeeksforGeeks");
+var printedPassword = document.querySelector('#password')
+
+
+// SET PASSWORD STRING
+var passwordCriteria = "";
+
+// GET CRITERIA
+
+// PUT CRITERIA INTO A STRING
+// number
+function randomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
+// lowercase
+function randomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+// uppercase
+function randomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+// special symbol
+function randomSymbol() {
+    return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+}
+// test the results
+console.log(randomNumber());
+console.log(randomLower());
+console.log(randomUpper());
+console.log(randomSymbol());
+// PUSH CRITERIA TOGETHER
+
+// PRINT PASSWORD TO PAGE
+
+
+
+
+// START FUNCTION
+
+
+// WANT PASSWORD TO PRINT ON PAGE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// getCriteria = () =>{
+//     var numbers = "0123456789";
+//     var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     var lower = "abcdefghijklmnopqrstuvwxyz";
+//     var special = "!@#$%^&*()";
+//     return
+//     password = [numbers, upper, lower, special]
+// }
+
+
+// startFunc = () => {
+//     prompt(getCriteria);
+// }
