b7b1afacebd25e4a426730c7925b515bfabfa74f
nbrown225 <chbrown225@yahoo.com>
2022-08-09 08:04:01 -0700

    able to get value from prompt

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,27 +1,75 @@
-// DOM ELEMENTS
-var startBtn = document.querySelector("#generate")
-var passwordLength = 0;
-var newPassword = "";
+var generateBtn = document.querySelector('#generate');
+var passwordText = document.querySelector('#password');
+var passwordLow = [];
 
-function includeUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-};
+var numbersArr = ['0','1','2','3','4','5','6','7','8','9']
 
-// STRING OF 128 FROM RANDOM RETURN
+var symbolArr = [,'$','%','^','&','*','(',')','#','@','!',]
+var UPPERArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
+var lowerArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
+// const MIN = 1;
+// const MAX = 10;
 
-// CRITERIA QUESTIONS
-function criteria () {
-    var confirmUpper = confirm("do you want to add upper?");
+
+
+function getLower() {
+    // var howLong = (prompt('8-140'));
+    
+    var lower = '';
+    if (confirm("lowercase characters?") == true) {
+        lower = lowerArr;
+        console.log(lower);
+        console.log(lowerArr);
+        passwordLow.push(lower);
+        getNum()
+    } else{
+        console.log('we wont add');
+        getNum()
+
+    }
+    console.log(passwordLow);
+    return passwordLow
 }
-for (var i = 0; i < parseInt(128); i++) {
-    if (confirmUpper) {
-    console.log(includeUpper());
-    } 
-    //IF YES, ADD
-    //IF NO, NEXT QUESTION
 
-//STRINGIFY THEM TOGETHER
+function getNum() {
+    var numbers = '';
+    if (confirm('numbers?') == true) {
+        numbers = numbersArr;
+        console.log(numbers);
+        randomize()
+    } else{
+        console.log('numbers not added');
+    }
+    console.log(passwordLow);
+    return passwordLow.push(numbers)
+}
+var temppass = ['0','1','2','3','4','5','6','7','8','9'];
+function getLength() {
+    var passwordLength = prompt('1-10')
+    passwordLength.value
+    console.log(passwordLength);
+    // var pwlength = parseInt(passwordLength.value)
+    // if (pwlength < MIN || pwlength > MAX) {
+    //     console.log(passwordLength);
+    // }
+    // return
+    
+}
 
-//WRITE PASSWORD
-console.log(includeUpper);
-startBtn.addEventListener("click",criteria)
+// function pwLenght() {
+//     characterLength = parseInt(prompt('How long do you want the password? 8 - 140'));
+//     if(isNaN(characterLength) || characterLength<8 || characterLength<140){
+//         return false;
+//     }
+
+// }
+
+
+
+function writePassword() {
+    console.log("clicked");
+    getLength()
+}
+
+
+//generateBtn.addEventListener('click', writePassword);
